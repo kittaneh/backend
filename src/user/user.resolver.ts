6611faf22b1d7cd02657f668ab5e2ce6ad43fc4e
Yahya Daoud <kittaneh@gmail.com>
2020-11-14 20:43:48 +0200

    serverside validation

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,6 +1,15 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { UserService } from './user.service';
 import { User } from './user.model';
+import * as Joi from '@hapi/joi';
+import { BadRequestException } from '@nestjs/common';
+
+//server validation with Joi
+const schema = Joi.object().keys({
+  firstname: Joi.string().required(),
+  lastname: Joi.string().required(),
+  email: Joi.string().email().required(),
+});
 
 @Resolver('User')
 export class UserResolver {
@@ -22,6 +31,14 @@ export class UserResolver {
     @Args('lastname') lastname: string,
     @Args('email') email: string,
   ) {
+    const result = schema.validate({
+      firstname: firstname,
+      lastname: lastname,
+      email: email,
+    });
+    if (result.error) {
+      throw new BadRequestException(result.error);
+    }
     return await this.userService.create(firstname, lastname, email);
   }
 }
